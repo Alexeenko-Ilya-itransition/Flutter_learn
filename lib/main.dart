e0f05ce0d4a468da70b619c7f497433d5400e2a7
Alexeenko Ilya <alexeenko.ilya55@gmail.com>
2022-09-08 15:37:21 +0300

    Add Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,60 +12,56 @@ class MyApp extends StatefulWidget {
   State<StatefulWidget> createState() => _MyAppState();
 }
 
-  class _MyAppState extends State<MyApp>{
-
+class _MyAppState extends State<MyApp> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
-  Widget build(BuildContext context) {
-  return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: const Text('My first app'), centerTitle: true),
-        drawer: Drawer(
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: [
-          const DrawerHeader(
-          decoration: BoxDecoration(
-          color: Colors.blue,
+  Widget build(BuildContext ctx) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(title: const Text('My first app'), centerTitle: true),
+          drawer: Drawer(
+            child: ListView(padding: EdgeInsets.zero, children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+                    Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2 '),
+                onTap:(){
+                    Navigator.pop(context);
+                }
+              ),
+            ]),
           ),
-          child: Text('Drawer Header'),
-        ),
-          ListTile(
-            title: const Text('Item 1'),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
+          body: ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemBuilder: (context, i) {
+                if (i.isOdd) return const Divider();
+                final index = i ~/ 2;
+                if (index >= _suggestions.length) {
+                  _suggestions.addAll(generateWordPairs().take(10));
+                }
+                return ListTile(
+                    title: Text(
+                  _suggestions[index].asPascalCase,
+                  style: _biggerFont,
+                ));
+              }),
+          floatingActionButton: FloatingActionButton(
+            child: const Text('button'),
+            onPressed: () {},
           ),
-          ListTile(
-            title: const Text('Item 2'),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
-        ),]),),
-        body: ListView.builder(
-            padding: const EdgeInsets.all(16.0),
-            itemBuilder: (context,i){
-              if(i.isOdd) return const Divider();
-              final index = i ~/ 2;
-              if(index >= _suggestions.length){
-                _suggestions.addAll(generateWordPairs().take(10));
-              }
-          return ListTile(
-            title: Text(_suggestions[index].asPascalCase,
-            style: _biggerFont,
-            )
-          );
-        }),
-        floatingActionButton: FloatingActionButton(
-          child: const Text('button'),
-          onPressed: () {},
-        ),
-      ));
+        ));
   }
 }
